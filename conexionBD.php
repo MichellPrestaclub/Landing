cb8d8b4ded74ed7e320023c224c81d83efdbbcb1
MIchell Pacheco T <MP717786@PRESTACLUBSAC.LOCAL>
2022-07-19 15:40:43 -0500

    Agregue comando conexion base de datos

--- conexionBD.php
+++ conexionBD.php
@@ -1,23 +1,33 @@
 <?php
-  $servername = "mysql.hostinger.co.uk";
-  $database = "u266072517_name";
-  $username = "u266072517_user";
-  $password = "buystuffpwd";
+$servername = "localhost";
+$database = "test";
+$username = "root";
+$password = "";
 // Create connection
-
-  $conn = mysqli_connect($servername, $username, $password, $database);
+?>
+<html>
+<?php
+$conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
 
-  if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-  }
- 
-  echo "Connected successfully";
- 
-  $sql = "INSERT INTO Students (name, lastname, email) VALUES ('Test', 'Testing', 'Testing@tesing.com')";
-  if (mysqli_query($conn, $sql)) {
-      echo "New record created successfully";
-  } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  mysqli_close($conn);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully <br>";
+$nombre = 'Prueba';
+$dni = '423223';
+$correo = 'prueba@gmail.com';
+$celular = '999888777';
+$fecha = '2022-12-03';
+
+$sql = "INSERT INTO clientelanding (id,nombres,dni,correo,celular,fecha) VALUES (NULL,'$nombre','$dni','$correo','$celular','$fecha');";
+if (($result = mysqli_query($conn, $sql)) === false) {
+    die(mysqli_error($conn));
+} else {
+    echo "<br>Logrado: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
+
+</html>
