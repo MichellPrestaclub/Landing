7ddb158dc0ccbe8d68b2ab5a9c135c73485119e8
MIchell Pacheco T <MP717786@PRESTACLUBSAC.LOCAL>
2022-07-21 18:14:59 -0500

    Nueva version

--- conexionBD.php
+++ conexionBD.php
@@ -4,30 +4,11 @@ $database = "test";
 $username = "root";
 $password = "";
 // Create connection
-?>
-<html>
-<?php
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
+    echo "Sus datos no lograron ser enviados correctamente";
 }
-
-echo "Connected successfully <br>";
-$nombre = 'Prueba';
-$dni = '423223';
-$correo = 'prueba@gmail.com';
-$celular = '999888777';
-$fecha = '2022-12-03';
-
-$sql = "INSERT INTO clientelanding (id,nombres,dni,correo,celular,fecha) VALUES (NULL,'$nombre','$dni','$correo','$celular','$fecha');";
-if (($result = mysqli_query($conn, $sql)) === false) {
-    die(mysqli_error($conn));
-} else {
-    echo "<br>Logrado: " . $sql . "<br>" . mysqli_error($conn);
-}
-mysqli_close($conn);
 ?>
-
-</html>
---
+++ header.php
@@ -0,0 +1,14 @@
+<header class="header">
+      <nav class="nav">
+        <a href="#"><img src="img_menu/logo_burger.png" alt=""></a>
+        <button type="button" class="icon_nav" aria-label="Abrir menu"><i class="fas fa-bars"></i></button>
+        <ul class="nav_menu">
+          <li class="nav_menu_item"> <a href="nosotros/index_nosotros.html" class="nav_menu_link">Nosotros</a></li>
+          <li class="nav_menu_item"> <a href="#promociones" class="nav_menu_link_promociones">Promociones</a></li>
+          <li class="nav_menu_item"> <a href="carta/index_carta.html" class="nav_menu_link">Nuestra carta</a></li>
+          <li class="nav_menu_item"> <a href="contacto.html" class="nav_menu_link">Contactenos</a></li>
+          <li class="nav_menu_item"> <a href="login.html" class="nav_menu_link">Intranet</a></li>
+          <li class="nav_menu_item"> <a href="#" class="nav_menu_link_carrito">Carrito</a></li>
+        </ul>
+      </nav>
+</header>
